678ebab267007f019026183d97cea0a90c8adf16
Schep336 <58539679+Schep336@users.noreply.github.com>
2020-01-18 22:49:27 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,110 @@
+var specialCharacters = [
+  "@",
+  "%",
+  "+",
+  "\\",
+  "/",
+  "'",
+  "!",
+  "#",
+  "$",
+  "^",
+  "?",
+  ":",
+  ",",
+  ")",
+  "(",
+  "}",
+  "{",
+  "]",
+  "[",
+  "~",
+  "-",
+  "_",
+  "."
+];
+var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowerCasedCharacters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z"
+];
+var upperCasedCharacters = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z"
+];
+
+document
+.getElementById("generateButton")
+.addEventListener("click", function(e) {
+    e.preventDefault();
+    const password = generateButton();
+    document.getElementById("password").value = password;
+
+     });
+
+function generateButton () {
+let values = "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
+let password = ""; 
+    
+    for(var i = 0; i <= 25; i++) {
+        password = password + values.charAt(Math.floor(Math.random() * (values.length - 1)));
+      }
+      return password;
+    }
+    
+     
+function copy(){
+
+     document.getElementById("password").select();
+     document.execCommand("copy");
+     
+        }
+        
